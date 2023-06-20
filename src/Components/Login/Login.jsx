ef2f51f29adf7c51ba2891f2689f304b586cca2b
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 12:10:17 -0300

    Criando o Routes dentro do Login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Login;

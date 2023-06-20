44ad674ae6636b34064bdf5fb61f992b73aa69a6
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 12:26:28 -0300

    Exemplo de uso LoginForm

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,13 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useContext('');
-  const [password, setPassword] = React.useContext('');
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((r) => {
+        console.log(r);
+        return r.json();
+      })
+      .then((json) => console.log(json));
+  }
 
   return (
     <section>
       <h1>Login</h1>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={username}

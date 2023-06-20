6fcd9059d83870b86ef232295a663f5816e0657e
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 10:07:43 -0300

    Adicionando o react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }

06d9ea15c610169a4fbe521601f4c39dec71c981
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 12:16:08 -0300

    Criando o Link dentro do LoginForm para as outras rotas

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <div>
+      <Link to="/login/criar">Cadastro</Link>
+    </div>
+  );
 };
 
 export default LoginForm;

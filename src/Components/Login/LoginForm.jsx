807955714dc57c204a7f1c43d2dca153e755e388
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 12:17:33 -0300

    Consertando caminho das rotas no App.jsx

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -5,6 +5,8 @@ const LoginForm = () => {
   return (
     <div>
       <Link to="/login/criar">Cadastro</Link>
+      <Link to="/login/perdeu">Perdeu</Link>
+      <Link to="/login/resetar">Resetar</Link>
     </div>
   );
 };

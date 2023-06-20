40aef3e2daa203953eac1ae487c96e6b5a6ac00e
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 16:11:22 -0300

    Utilizando as funções do UserContext

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Dogs } from '../Assets/dogs.svg';
+import { UserContext } from '../UserContext';
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <div className={styles.header}>
       <nav className={`${styles.nav} container`}>
--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -26,7 +26,7 @@ export const UserStorage = ({ children }) => {
   }
 
   return (
-    <UserContext.Provider value={{ userLogin }}>
+    <UserContext.Provider value={{ userLogin, data }}>
       {children}
     </UserContext.Provider>
   );

20c327576e81da395413e3968e8b7365b85159aa
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 10:10:18 -0300

    Criando Link no Header com react-router-dom

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;

db7385a92b031923f7508215e3035a2b57b37139
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 14:39:27 -0300

    onBlur

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({ label, type, name, value, onChange, error }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
@@ -14,6 +14,7 @@ const Input = ({ label, type, name, value, onChange, error }) => {
         type={type}
         value={value}
         onChange={onChange}
+        onBlur={onBlur}
       />
       {error && <p className={styles.error}>{error}</p>}
     </div>

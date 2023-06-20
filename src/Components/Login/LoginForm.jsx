e591cece8da771b8d44c6a2ccda0f21b2d0441f8
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 15:41:38 -0300

    function getUser

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
-import { TOKEN_POST } from '../../api';
+import { TOKEN_POST, USER_GET } from '../../api';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -21,6 +28,7 @@ const LoginForm = () => {
       const response = await fetch(url, options);
       const json = await response.json();
       window.localStorage.setItem('token', json.token);
+      getUser(json.token);
     }
   }
 

4af2b6e8e3bd3d180b91e70e156c67c9ecbe8341
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 12:20:23 -0300

    Estrutura Inicial do LoginForm

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
+  const [username, setUsername] = React.useContext('');
+  const [password, setPassword] = React.useContext('');
+
   return (
-    <div>
+    <section>
+      <h1>Login</h1>
+      <form action="">
+        <input
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Entrar</button>
+      </form>
       <Link to="/login/criar">Cadastro</Link>
       <Link to="/login/perdeu">Perdeu</Link>
       <Link to="/login/resetar">Resetar</Link>
-    </div>
+    </section>
   );
 };
 

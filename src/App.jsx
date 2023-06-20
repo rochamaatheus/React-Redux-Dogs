5c506670dac8fe6cf1aedcc1b61315e3c90f2d51
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 10:04:49 -0300

    Importando Header e Footer ao App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import React from 'react';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
 function App() {
-  return <div>App React</div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;

ca25e6997cf190c2eae7161730886393b7970738
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-20 10:12:04 -0300

    Route - Login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <div className={styles.header}>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
+        <Link to="/login">Login / Criar</Link>
       </nav>
     </div>
   );

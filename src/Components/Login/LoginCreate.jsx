03b1350806510be3971d8c4fea3392c8943b8f93
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-21 17:10:28 -0300

    Function handleSubmit

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const LoginCreate = () => {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <section className="animeLeft">
       <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}></form>
     </section>
   );
 };

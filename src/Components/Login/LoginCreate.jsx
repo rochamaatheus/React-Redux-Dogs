52e57b8a7d7746e6030ba6ecb0b6b3d421eadb7f
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-21 17:40:23 -0300

    USER_POST

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
+import { USER_POST } from '../../api';
 
 const LoginCreate = () => {
   const username = useForm();
   const email = useForm('email');
   const password = useForm('password');
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value,
+    });
+    const response = await fetch(url, options);
   }
 
   return (

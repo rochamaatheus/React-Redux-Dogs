ff086f623dc8d2bf526e1e5f2cee135f7fcc62fb
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-21 17:11:58 -0300

    Import dos Forms

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
 
 const LoginCreate = () => {
   function handleSubmit(event) {
@@ -8,7 +10,10 @@ const LoginCreate = () => {
   return (
     <section className="animeLeft">
       <h1 className="title">Cadastre-se</h1>
-      <form onSubmit={handleSubmit}></form>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" />
+        <Button>Cadastrar</Button>
+      </form>
     </section>
   );
 };

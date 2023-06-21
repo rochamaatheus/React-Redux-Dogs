401f0f93f771f1858cd996246c2f51e0f66698c2
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-21 17:09:47 -0300

    Adicionando o HTML

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const LoginCreate = () => {
-  return <div>LoginCreate</div>;
+  return (
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+    </section>
+  );
 };
 
 export default LoginCreate;

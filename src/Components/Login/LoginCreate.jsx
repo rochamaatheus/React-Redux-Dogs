f08045a93fb1f2a2e20a97c5c6de0948372cf198
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-21 17:15:07 -0300

    Importando e usando o Hook useForm

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
 
 const LoginCreate = () => {
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm('password');
+
   function handleSubmit(event) {
     event.preventDefault();
   }
@@ -11,7 +16,9 @@ const LoginCreate = () => {
     <section className="animeLeft">
       <h1 className="title">Cadastre-se</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Cadastrar</Button>
       </form>
     </section>

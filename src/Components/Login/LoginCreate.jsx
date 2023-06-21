6dfef941a5a7145992b13d7a4bba4e2017e3346e
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-21 17:42:09 -0300

    UserContext - userLogin

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -3,11 +3,14 @@ import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
 import { USER_POST } from '../../api';
+import { UserContext } from '../../UserContext';
 
 const LoginCreate = () => {
   const username = useForm();
   const email = useForm('email');
   const password = useForm('password');
+
+  const { userLogin } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -17,6 +20,9 @@ const LoginCreate = () => {
       password: password.value,
     });
     const response = await fetch(url, options);
+    if (response.ok) {
+      userLogin(username.value, password.value);
+    }
   }
 
   return (

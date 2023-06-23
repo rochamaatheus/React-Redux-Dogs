a20705123b82ac4d848f3aba0533ea81ea1ab960
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-23 15:22:16 -0300

    Criando os arquivos

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import UserHeader from './UserHeader';
+import { Route, Routes } from 'react-router-dom';
 
 const User = () => {
-  return <div>User</div>;
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+      </Routes>
+    </section>
+  );
 };
 
 export default User;

9e68face054323588276eb3d42c32ac678af4131
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-23 18:41:10 -0300

    Utilizando o useForm

--- src/Components/User/UserPhotoPost.jsx
+++ src/Components/User/UserPhotoPost.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import styles from './UserPhotoPost.module.css';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
 
 const UserPhotoPost = () => {
+  const nome = useForm();
+  const peso = useForm('number');
+  const idade = useForm('number');
+
   function handleSubmit(event) {
     event.preventDefault();
   }
@@ -14,7 +19,7 @@ const UserPhotoPost = () => {
     <section className={`${styles.photoPost} animeLeft`}>
       <form onSubmit={handleSubmit}>
         <Input label="Nome" type="text" name="nome" />
-        <Input label="Peso" type="text" name="peso" />
+        <Input label="Peso" type="number" name="peso" />
         <Input label="Idade" type="number" name="idade" />
         <input type="file" name="img" id="img" onChange={handleImgChange} />
         <Button>Enviar</Button>

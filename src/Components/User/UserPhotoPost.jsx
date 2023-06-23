091fa3742a0acf990c05576045683e66155b13b7
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-23 18:53:24 -0300

    Fazendo o request

--- src/Components/User/UserPhotoPost.jsx
+++ src/Components/User/UserPhotoPost.jsx
@@ -4,6 +4,7 @@ import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
 import useFetch from '../../Hooks/useFetch';
+import { PHOTO_POST } from '../../api';
 
 const UserPhotoPost = () => {
   const nome = useForm();
@@ -19,6 +20,11 @@ const UserPhotoPost = () => {
     formData.append('nome', nome.value);
     formData.append('peso', peso.value);
     formData.append('idade', idade.value);
+
+    const token = window.localStorage.getItem('token');
+    if (!token) return;
+    const { url, options } = PHOTO_POST(formData, token);
+    request(url, options);
   }
 
   function handleImgChange({ target }) {
@@ -30,9 +36,9 @@ const UserPhotoPost = () => {
   return (
     <section className={`${styles.photoPost} animeLeft`}>
       <form onSubmit={handleSubmit}>
-        <Input label="Nome" type="text" name="nome" />
-        <Input label="Peso" type="number" name="peso" />
-        <Input label="Idade" type="number" name="idade" />
+        <Input label="Nome" type="text" name="nome" {...nome} />
+        <Input label="Peso" type="number" name="peso" {...peso} />
+        <Input label="Idade" type="number" name="idade" {...idade} />
         <input type="file" name="img" id="img" onChange={handleImgChange} />
         <Button>Enviar</Button>
       </form>

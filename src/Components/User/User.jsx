96a6dd3a233f10b19b45bd0f4925a7dff9a5ef06
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-23 15:23:03 -0300

    Criando e importando o Feed

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserHeader from './UserHeader';
 import { Route, Routes } from 'react-router-dom';
+import Feed from '../Feed/Feed';
 
 const User = () => {
   return (

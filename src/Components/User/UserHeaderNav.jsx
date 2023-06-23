8fe5f7ec7a1c661ae8d1d4e0af74f18df02e1d34
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-23 15:36:21 -0300

    Adicionando SVGs

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
+import { ReactComponent as MinhasFotos } from '../../Assets/feed.svg';
+import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
+import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
+import { ReactComponent as Sair } from '../../Assets/sair.svg';
 
 const UserHeaderNav = () => {
   const { userLogout } = React.useContext(UserContext);
 
   return (
     <nav>
-      <NavLink to="/conta">Minhas Fotos</NavLink>
-      <NavLink to="/conta/estatisticas">Estatísticas</NavLink>
-      <NavLink to="/conta/postar">Adicionar Foto</NavLink>
-      <button onClick={userLogout}>Sair</button>
+      <NavLink to="/conta">
+        <MinhasFotos /> Minhas Fotos
+      </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+        Estatísticas
+      </NavLink>
+      <NavLink to="/conta/postar">
+        <AdicionarFoto />
+        Adicionar Foto
+      </NavLink>
+      <button onClick={userLogout}>
+        <Sair />
+        Sair
+      </button>
     </nav>
   );
 };

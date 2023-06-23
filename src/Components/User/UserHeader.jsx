87105888ac3b4d61d0b54ac5baee82621b4311fb
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-23 15:54:22 -0300

    useLocation

--- src/Components/User/UserHeader.jsx
+++ src/Components/User/UserHeader.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import UserHeaderNav from './UserHeaderNav';
 import styles from './UserHeader.module.css';
+import { useLocation } from 'react-router-dom';
 
 const UserHeader = () => {
+  const [title, setTitle] = React.useState('');
+  const location = useLocation();
+
   return (
     <header className={styles.header}>
-      <h1 className="title">Título</h1>
+      <h1 className="title">{title}</h1>
       <UserHeaderNav />
     </header>
   );

319dae0feceda70ad8a44cd58e76d1e274ef0a7d
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-23 17:11:19 -0300

    Utilizando o useMedia

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -6,11 +6,13 @@ import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
 import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
 import { ReactComponent as Sair } from '../../Assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
+import useMedia from '../../Hooks/useMedia';
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null);
   const { userLogout } = React.useContext(UserContext);
   const navigate = useNavigate();
+
+  const mobile = useMedia('(max-width: 40rem)');
 
   function handleLogout() {
     userLogout();

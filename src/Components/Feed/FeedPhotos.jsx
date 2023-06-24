6dc19a6e9f7b726afe5ceb7793af4eb0275095b8
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-24 20:00:30 -0300

    Utilizando useFetch

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import FeedPhotosItem from './FeedPhotosItem';
+import useFetch from '../../Hooks/useFetch';
 
 const FeedPhotos = () => {
+  const { data, loading, error, request } = useFetch();
+
   return (
     <div>
       <FeedPhotosItem />

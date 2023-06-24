e26daf879f78767c276c13b43972def400e8d32b
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-24 20:01:51 -0300

    Função inicial

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import FeedPhotosItem from './FeedPhotosItem';
 import useFetch from '../../Hooks/useFetch';
+import { PHOTOS_GET } from '../../api';
 
 const FeedPhotos = () => {
   const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET();
+      request(url, options);
+    }
+    fetchPhotos();
+  }, [request]);
 
   return (
     <div>

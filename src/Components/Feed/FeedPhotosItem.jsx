1a1103c0efb6bc1131c7ae5f00c3a69c4245957f
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-24 20:21:26 -0300

    Return das photos

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const FeedPhotosItem = () => {
-  return <div>FeedPhotosItem</div>;
+const FeedPhotosItem = ({ photo }) => {
+  return (
+    <li>
+      <img src={photo.src} alt={photo.title} />
+      <span>{photo.acessos}</span>
+    </li>
+  );
 };
 
 export default FeedPhotosItem;

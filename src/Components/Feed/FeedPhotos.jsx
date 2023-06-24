e8007b9efc3c8cf74679c87e776e52f18e864b47
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-24 20:13:30 -0300

    Estrutura do request

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import FeedPhotosItem from './FeedPhotosItem';
 import useFetch from '../../Hooks/useFetch';
 import { PHOTOS_GET } from '../../api';
+import Error from '../Helper/Error';
 
 const FeedPhotos = () => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET();
-      request(url, options);
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { respone, json } = await request(url, options);
     }
     fetchPhotos();
   }, [request]);
 
+  if (error) return <Error error={error} />;
   return (
     <div>
       <FeedPhotosItem />

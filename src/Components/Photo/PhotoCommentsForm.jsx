2fb1ee5f606eaae2117eec791ebcac3e9a8b9570
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-25 18:07:58 -0300

    useFetch e function handleSubmit

--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
+import useFetch from '../../Hooks/useFetch';
 
 const PhotoCommentsForm = ({ id }) => {
   const [comment, setComment] = React.useState('');
+  const { request, error } = useFetch();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <textarea
         value={comment}
+        id="comment"
+        name="comment"
+        placeholder="Digite um comentário"
         onChange={({ target }) => setComment(target.value)}
       />
       <button>

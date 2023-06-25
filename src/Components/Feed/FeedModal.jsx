70596cfa613976a70b6df62929688d652b8fcf6d
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-25 15:47:56 -0300

    useEffect

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -4,6 +4,12 @@ import useFetch from '../../Hooks/useFetch';
 
 const FeedModal = ({ photo }) => {
   const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
   return <div className={styles.modal}>FeedModal</div>;
 };
 

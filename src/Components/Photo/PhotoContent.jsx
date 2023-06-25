3e1875673c8c88fbf0a05faacfb282d697154fad
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-25 16:05:35 -0300

    Criando css, importando e adicionando as classes

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import styles from './PhotoContent.module.css';
 
-const PhotoContent = () => {
-  return <div>PhotoContent</div>;
+const PhotoContent = ({ data }) => {
+  const { photo, comments } = data;
+
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.datails}></div>
+    </div>
+  );
 };
 
 export default PhotoContent;

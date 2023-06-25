e1a230afe84cac61c75dd069e10c351b3db30862
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-25 16:09:52 -0300

    Mais classes e tags

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './PhotoContent.module.css';
+import { Link } from 'react-router-dom';
 
 const PhotoContent = ({ data }) => {
   const { photo, comments } = data;
@@ -9,7 +10,22 @@ const PhotoContent = ({ data }) => {
       <div className={styles.img}>
         <img src={photo.src} alt={photo.title} />
       </div>
-      <div className={styles.datails}></div>
+      <div className={styles.datails}>
+        <div>
+          <p>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.views}></span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso}</li>
+            <li>{photo.idade}</li>
+            <li>{photo.peso}</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };

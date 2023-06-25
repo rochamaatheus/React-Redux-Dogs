a7b0296356c176bee6b16075109eb724533d18f6
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-25 18:02:06 -0300

    Criando PhotoCommentsForm e utilizando useContext

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
 
 const PhotoComments = ({ id, comments }) => {
-  return <div></div>;
+  const { login } = React.useContext(UserContext);
+
+  return <div>{login && <PhotoCommentsForm />}</div>;
 };
 
 export default PhotoComments;

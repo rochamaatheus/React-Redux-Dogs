e28a08883418a21cb83b7e2677b02268055cd3da
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-25 15:58:00 -0300

    Adicionando lógica

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './FeedModal.module.css';
 import useFetch from '../../Hooks/useFetch';
 import { PHOTO_GET } from '../../api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
 
 const FeedModal = ({ photo }) => {
   const { data, error, loading, request } = useFetch();
@@ -11,7 +13,13 @@ const FeedModal = ({ photo }) => {
     request(url, options);
   }, [photo, request]);
 
-  return <div className={styles.modal}>FeedModal</div>;
+  return (
+    <div className={styles.modal}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;

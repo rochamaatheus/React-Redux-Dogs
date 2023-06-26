7a0a730868e3f7ce12edd5c64218139403384a82
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-26 16:38:18 -0300

    Adicionando as classes

--- src/Components/Helper/Image.jsx
+++ src/Components/Helper/Image.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './Image.module.css';
 
-const Image = () => {
-  return <div className={styles.wrapper}>Image</div>;
+const Image = ({ alt, ...props }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.skeleton}></div>
+      <img className={styles.img} src="" alt="" />
+    </div>
+  );
 };
 
 export default Image;

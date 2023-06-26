87dd411614d1cf6aaebedeb08e2dae17109f9011
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-26 15:44:42 -0300

    Retornando apenas o button

--- src/Components/Photo/PhotoDelete.jsx
+++ src/Components/Photo/PhotoDelete.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import styles from './PhotoDelete.module.css';
 
 const PhotoDelete = ({ id }) => {
-  return (
-    <div>
-      <button className={styles.delete}>Deletar</button>
-    </div>
-  );
+  return <button className={styles.delete}>Deletar</button>;
 };
 
 export default PhotoDelete;

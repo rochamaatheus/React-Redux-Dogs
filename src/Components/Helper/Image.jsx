e6bc3ef33c4c5cb174c3f486bd1b0d8f2398f187
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-26 16:56:54 -0300

    function handleLoad

--- src/Components/Helper/Image.jsx
+++ src/Components/Helper/Image.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import styles from './Image.module.css';
 
 const Image = ({ alt, ...props }) => {
+  function handleLoad({ target }) {
+    target.style.opacity = 1;
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.skeleton}></div>
-      <img className={styles.img} alt={alt} {...props} />
+      <img onLoad={handleLoad} className={styles.img} alt={alt} {...props} />
     </div>
   );
 };

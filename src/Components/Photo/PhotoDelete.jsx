3ff0330d6497174115c5a33f82d3a30934608880
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-26 15:48:04 -0300

    handleClick function e token

--- src/Components/Photo/PhotoDelete.jsx
+++ src/Components/Photo/PhotoDelete.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import styles from './PhotoDelete.module.css';
+import { PHOTO_DELETE } from '../../api';
 
 const PhotoDelete = ({ id }) => {
-  return <button className={styles.delete}>Deletar</button>;
+  function handleClick(event) {
+    event.preventDefault();
+    const token = window.localStorage.getItem('token');
+    if (!token) return;
+    const { url, options } = PHOTO_DELETE(id, token);
+  }
+
+  return (
+    <button onClick={handleClick} className={styles.delete}>
+      Deletar
+    </button>
+  );
 };
 
 export default PhotoDelete;

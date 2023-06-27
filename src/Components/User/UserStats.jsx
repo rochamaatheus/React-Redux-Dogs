8141ce8ea4436878fe1d1f47418d04c13933dfef
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-27 19:29:58 -0300

    Lógica inicial

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
 
 const UserStats = () => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = {};
+    }
+  }, []);
+
   return (
     <div>
       <Head

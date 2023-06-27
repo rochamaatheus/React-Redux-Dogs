836b9728485a8aed7de91224cc51e3bf141b9f01
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-27 16:53:12 -0300

    Lógica inicial

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
 
 const LoginPasswordLost = () => {
-  return <div>LoginPasswordLost</div>;
+  const email = useForm();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <section onSubmit={handleSubmit}>
+      <h1 className="title">Perdeu a senha?</h1>
+      <form>
+        <Input label="Email / Usuário" type="text" name="email" {...email} />
+        <Button>Enviar Email</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginPasswordLost;

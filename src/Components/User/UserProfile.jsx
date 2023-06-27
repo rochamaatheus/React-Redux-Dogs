684a9e095e42a4ed9c3dda3a4e1e3ea4622fc962
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-27 16:22:22 -0300

    Importando Feed

--- src/Components/User/UserProfile.jsx
+++ src/Components/User/UserProfile.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Feed from '../Feed/Feed';
 
 const UserProfile = () => {
   const { user } = useParams();
-  return <div>UserProfile</div>;
+  return (
+    <div>
+      <Feed user={user} />
+    </div>
+  );
 };
 
 export default UserProfile;

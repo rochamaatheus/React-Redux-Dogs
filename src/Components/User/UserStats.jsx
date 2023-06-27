f6829f1954a5a177a9ee244bc29f60e22a57ef95
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-27 19:39:48 -0300

    Estilos

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -28,7 +28,7 @@ const UserStats = () => {
           title="Estatísticas"
           description="Visualize duas estatísticas-chave em nosso site por meio de dois gráficos claros. Obtenha uma visão rápida e fácil do número de usuários ativos e do engajamento do site ao longo do tempo. Acompanhe o crescimento do nosso público e veja como as interações aumentam ao longo do período. Tome decisões embasadas com base nessas informações essenciais apresentadas de forma acessível"
         />
-        <UserStatsGraphs />
+        <UserStatsGraphs data={data} />
       </div>
     );
   else return null;
--- src/Components/User/UserStatsGraphs.jsx
+++ src/Components/User/UserStatsGraphs.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styles from './UserStatsGraphs.module.css';
 
-const UserStatsGraphs = () => {
-  return <div>UserStatsGraphs</div>;
+const UserStatsGraphs = ({ data }) => {
+  const [graph, setGraph] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
+
+  return (
+    <section className={`${styles.graph} animeLeft`}>UserStatsGraphs</section>
+  );
 };
 
 export default UserStatsGraphs;

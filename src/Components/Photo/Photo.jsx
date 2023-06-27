63658e29d545b533c3792f5c055d5e0b1f759fd5
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-27 15:18:16 -0300

    Importando useFetch

--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useFetch from '../../Hooks/useFetch';
 
 const Photo = () => {
   const { id } = useParams();
+  const { data, loading, error, request } = useFetch();
 
   return <div>{id}</div>;
 };

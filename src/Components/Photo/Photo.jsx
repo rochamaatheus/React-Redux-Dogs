927f636e061feb211c593df19abb89ee4b621f0a
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-06-27 15:21:11 -0300

    usando PHOTO_GET

--- src/Components/Photo/Photo.jsx
+++ src/Components/Photo/Photo.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useFetch from '../../Hooks/useFetch';
+import { PHOTO_GET } from '../../api';
 
 const Photo = () => {
   const { id } = useParams();
   const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(id);
+  }, [id]);
 
   return <div>{id}</div>;
 };

2992011abe1b63a35b8e1b2ea3578300d659214e
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-07-20 11:59:27 -0300

    Importando o Reducer/photo na configureStore

--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -1,6 +1,7 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import photo from './photo';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({ photo });
 const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),

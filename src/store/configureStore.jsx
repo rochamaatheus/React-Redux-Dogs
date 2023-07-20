555d8de50a665aa2173ddbdf00b383d551dfc202
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-07-20 19:31:33 -0300

    Criando e importando o photoPost

--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -1,11 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import photo from './photo';
+import photoPost from './photoPost';
 import token from './token';
 import user from './user';
 import feed from './feed';
 import ui from './ui';
 
-const reducer = combineReducers({ photo, token, user, feed, ui });
+const reducer = combineReducers({ photo, photoPost, token, user, feed, ui });
 const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),

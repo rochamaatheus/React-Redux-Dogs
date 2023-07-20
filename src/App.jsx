14dc8fbdace1b7a0e7b1dc3a50dad81ec48fc5c8
Matheus Rocha <matheussilveirarocha.sc@gmail.com>
2023-07-20 15:40:18 -0300

    Adicionando o reset na função de logout

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
-import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Photo from './Components/Photo/Photo';
@@ -16,27 +15,25 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <UserStorage>
-          <Header />
-          <main className="AppBody">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="login/*" element={<Login />} />
-              <Route
-                path="conta/*"
-                element={
-                  <ProtectedRoute>
-                    <User />
-                  </ProtectedRoute>
-                }
-              />
-              <Route path="foto/:id" element={<Photo />} />
-              <Route path="perfil/:user" element={<UserProfile />} />
-              <Route path="*" element={<Error404 />} />
-            </Routes>
-          </main>
-          <Footer />
-        </UserStorage>
+        <Header />
+        <main className="AppBody">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+        </main>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- src/store/user.jsx
+++ src/store/user.jsx
@@ -1,6 +1,6 @@
 import createAsyncSlice from './helper/createAsyncSlice';
 import { USER_GET } from '../api';
-import { fetchToken } from './token';
+import { fetchToken, resetTokenState } from './token';
 
 const slice = createAsyncSlice({
   name: 'user',
@@ -9,9 +9,17 @@ const slice = createAsyncSlice({
 
 export const fetchUser = slice.asyncAction;
 
+const { resetState: resetUserState } = slice.actions;
+
 export const userLogin = (user) => async (dispatch) => {
   const { payload } = await dispatch(fetchToken(user));
   if (payload.token) await dispatch(fetchUser(payload.token));
+};
+
+export const userLogout = () => async (dispatch) => {
+  dispatch(resetUserState());
+  dispatch(resetTokenState());
+  window.localStorage.removeItem('token');
 };
 
 export default slice.reducer;
